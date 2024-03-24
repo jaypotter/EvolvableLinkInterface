fbc1e250adb83dedaa8b55d8139528b4e8058b9c
Jay Potter <j@ypotter.ca>
2024-03-24 19:35:31 -0400

    Stringable

--- src/Potter/Link/Evolvable/EvolvableLinkInterface.php
+++ src/Potter/Link/Evolvable/EvolvableLinkInterface.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Potter\Link\Evolvable;
 
-use \Psr\Link\EvolvableLinkInterface as PsrEvolvableLinkInterface;
+use \Psr\Link\EvolvableLinkInterface as PsrEvolvableLinkInterface, \Stringable;
 
 interface EvolvableLinkInterface extends PsrEvolvableLinkInterface
 {
-    public function withHref(string $href): static;
+    public function withHref(Stringable|string $href): static;
     public function withRel(string $rel): static;
     public function withoutRel(string $rel): static;
     public function withAttribute(string $attribute, string $value): static;
